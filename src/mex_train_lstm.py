3c798b16ce8af6187008f90cf3d1a387d406ed32
MysticShadow427 <devendrakayande427@gmail.com>
2024-04-27 23:01:24 +0530

    lstm code modified

--- src/mex_train_lstm.py
+++ src/mex_train_lstm.py
@@ -1,7 +1,8 @@
 from collections import defaultdict
 import argparse
 from mex_trainer import train_epoch_lstm,eval_model_lstm
-from mex_dataloader import create_data_loader
+from mex_augment_data import random_oversample_track_3,random_oversample
+from mex_dataloader import create_data_loader,create_data_loader_test
 from load_lstm import MexSpanClassifierLSTM
 from utils import plot_accuracy_loss,save_training_history
 from transformers import AutoModel,AutoTokenizer
@@ -11,8 +12,8 @@ import torch.nn as nn
 from torch.optim import Adam
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from mex_preprocess import remove_chars_except_punctuations,remove_newline_pattern,remove_numbers_and_urls,remove_pattern
-from mex_eval import get_confusion_matrix,get_predictions,get_scores,get_classification_report
+from mex_preprocess import remove_chars_except_punctuations,remove_newline_pattern,remove_numbers_and_urls,remove_pattern,remove_emojis
+from mex_eval import get_confusion_matrix,get_predictions,get_scores,get_classification_report,generate_submission_track_1
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -42,30 +43,33 @@ if __name__ == "__main__":
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print('\033[96m' + 'Device : ',device + '\033[0m')
     print()
+    train_df = pd.read_csv('/content/homo-mex-2024/data/public_data_train_phase/track_1_train.csv')
+    val_df = pd.read_csv('/content/homo-mex-2024/data/public_data_dev_phase/track_1_dev.csv')
+    test_df = pd.read_csv('/content/homo-mex-2024/data/public_data_test/track_1_public_test.csv')
+    # train_df.rename(columns={'lyrics': 'content'}, inplace=True)
+    # val_df.rename(columns={'lyric': 'content'}, inplace=True)
+    # test_df.rename(columns={'lyrics': 'content'}, inplace=True)
+    train_df['content'] = train_df['content'].apply(remove_pattern)
+    train_df['content'] = train_df['content'].apply(remove_numbers_and_urls)
+    train_df['content'] = train_df['content'].apply(remove_chars_except_punctuations)
+    train_df['content'] = train_df['content'].apply(remove_newline_pattern)
+    train_df['content'] = train_df['content'].apply(remove_emojis)
+    train_df = random_oversample(train_df)
 
-    df = pd.read_csv('/content/homo-mex-2024/data/public_data_dev_phase/track_1_dev.csv')
+    val_df['content'] = val_df['content'].apply(remove_pattern)
+    val_df['content'] = val_df['content'].apply(remove_numbers_and_urls)
+    val_df['content'] = val_df['content'].apply(remove_chars_except_punctuations)
+    val_df['content'] = val_df['content'].apply(remove_newline_pattern)
+    val_df['content'] = val_df['content'].apply(remove_emojis)
 
-    df['content'] = df['content'].apply(remove_pattern)
-    df['content'] = df['content'].apply(remove_numbers_and_urls)
-    df['content'] = df['content'].apply(remove_chars_except_punctuations)
-    df['content'] = df['content'].apply(remove_newline_pattern)
-    X = df['content']
-    y = df['label']
-
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42,stratify=y)
-
-    train_df = pd.DataFrame({'content': X_train, 'label': y_train})
-    val_df = pd.DataFrame({'content': X_val, 'label': y_val})
+    test_df['content'] = test_df['content'].apply(remove_pattern)
+    test_df['content'] = test_df['content'].apply(remove_numbers_and_urls)
+    test_df['content'] = test_df['content'].apply(remove_chars_except_punctuations)
+    test_df['content'] = test_df['content'].apply(remove_newline_pattern)
+    test_df['content'] = test_df['content'].apply(remove_emojis)
     
-    # test_df = pd.read_csv('/content/homo-mex-2024/data/public_data_test_phase/track_1_test.csv')
     print('\033[96m' + 'Loaded Training, validation and test dataframes'+ '\033[0m')
     print()
-
-
-    # test_df['content'] = test_df['content'].apply(remove_pattern)
-    # test_df['content'] = test_df['content'].apply(remove_numbers_and_urls)
-    # test_df['content'] = test_df['content'].apply(remove_chars_except_punctuations)
-    # test_df['content'] = test_df['content'].apply(remove_newline_pattern)
 
     print('\033[96m' + 'Preprocessing of Data done'+ '\033[0m')
     print()
@@ -90,7 +94,7 @@ if __name__ == "__main__":
 
     train_data_loader = create_data_loader(train_df,tokenizer=tokenizer,max_len=100,batch_size=batch_size)
     val_data_loader = create_data_loader(val_df,tokenizer=tokenizer,max_len=100,batch_size=batch_size)
-    # test_data_loader = create_data_loader(test_df,tokenizer=tokenizer,max_len=100,batch_size=batch_size)
+    test_data_loader = create_data_loader_test(test_df,tokenizer=tokenizer,max_len=100,batch_size=batch_size)
     print('\033[96m' + 'Dataloaders created')
     print()
 
@@ -170,15 +174,10 @@ if __name__ == "__main__":
 
     print('\033[96m' + 'Getting Predictions...'+ '\033[0m')
     print()
-    # y_review_texts_test, y_pred_test, y_pred_probs_test, y_test = get_predictions(model,test_data_loader)
+
     y_review_texts_val, y_pred_val, y_pred_probs_val, y_val = get_predictions(model,val_data_loader)
     y_review_texts_train, y_pred_train, y_pred_probs_train, y_train = get_predictions(model,train_data_loader)
 
-    # print('Test Data Classification Report : ')
-    # print()
-    # get_classification_report(y_test,y_pred_test)
-    # get_scores(y_test,y_pred_test)
-    # get_confusion_matrix(y_test,y_pred_test)
     print('\033[96m' + 'Val Data Classification Report : '+ '\033[0m')
     print()
     get_classification_report(y_val,y_pred_val)
@@ -189,5 +188,4 @@ if __name__ == "__main__":
     get_classification_report(y_train,y_pred_train)
     get_scores(y_train,y_pred_train)
     get_confusion_matrix(y_train,y_pred_train)
-
-# currently getting around 78-79% accuracy using this method as we are training on imbalanced data
+    generate_submission_track_1(model,test_data_loader,class_to_index)
